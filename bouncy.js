db1c28d380b100df2908aefbf1c6e90d2b9f669f
Matt Davis <mattdavis1337@gmail.com>
2014-01-31 12:04:16 -0600

    added sibsco record

--- bouncy.js
+++ bouncy.js
@@ -3,11 +3,17 @@ var bouncy = require('bouncy');
 var server = bouncy(function (req, res, bounce) {
   console.log('bouncy recieved request');
 
-  // Fancy way to check if user is navigating to this url
-  if ( ~req.headers.host.indexOf('apotheosisentertainment.com') ) {
+  if ( req.headers.host.toLowerCase().indexOf('www.apotheosisentertainment.com') != -1 ) {
     console.log('header has apotheosisentertainment.com')
     bounce(6007);
   }
+  else if (req.headers.host.toLowerCase().indexOf('www.sibsco.com') != -1 ) {
+    bounce(3456);
+  }
+  else{
+    bounce(6007);
+  }
+
 });
 
 server.listen(6008);
