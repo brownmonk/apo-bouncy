03c59e6608905f9a77303727bcb97d4cff49e1ab
Matt Davis <mattdavis1337@gmail.com>
2014-01-31 11:56:16 -0600

    logging

--- bouncy.js
+++ bouncy.js
@@ -1,10 +1,11 @@
 var bouncy = require('bouncy');
 
 var server = bouncy(function (req, res, bounce) {
-  console.log('bouncy');
+  console.log('bouncy recieved request');
 
   // Fancy way to check if user is navigating to this url
   if ( ~req.headers.host.indexOf('apotheosisentertainment.com') ) {
+    console.log('header has apotheosisentertainment.com')
     bounce(6007);
   }
 });
